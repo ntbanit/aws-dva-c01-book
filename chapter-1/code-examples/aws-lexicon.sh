f65b549f71559facf68bf647daea0e6cb28f37d3
an.nguyen <ntban.it@gmail.com>
2023-08-16 18:10:12 +0700

    sample code

--- chapter-1/code-examples/aws-lexicon.sh
+++ chapter-1/code-examples/aws-lexicon.sh
@@ -4,3 +4,6 @@ aws polly synthesize-speech --text 'Hello AWS World!' --voice-id Joanna --output
 
 # Trying again against a different Regional API endpoint
 aws polly synthesize-speech --text 'Hello AWS World' --voice-id Joanna --output-format mp3 hello-custom.mp3 --lexicon-names="awsLexicon" --region us-east-1
+
+aws polly delete-lexicon --name awsLexicon
+aws polly delete-lexicon --name awsLexicon --region eu-west-2
