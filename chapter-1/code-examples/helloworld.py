8951883bfbfa3c003a466ae3f280cc99ec0829c2
an.nguyen <ntban.it@gmail.com>
2023-08-21 22:08:42 +0700

    more exercise chapter 1

--- chapter-1/code-examples/helloworld.py
+++ chapter-1/code-examples/helloworld.py
@@ -2,9 +2,9 @@
 import boto3
 #Implicit Client Configuration
 polly = boto3.client('polly')
-result = polly.synthesize_speech(Text='Hello World!',
+result = polly.synthesize_speech(Text='Hello World but in a more fancy way with Polly',
 								OutputFormat='mp3',
-								VoiceId='Aditi')
+								VoiceId='Ivy')
 # Save the Audio from the response
 audio = result['AudioStream'].read()
 with open("helloworld.mp3","wb") as file:
